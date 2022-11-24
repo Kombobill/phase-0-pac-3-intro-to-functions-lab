4a69be04c2133b065f0f6a627be50917d216d7d5
Billgates <billmadru25@gmail.com>
2022-11-24 12:08:32 +0300

    Completed assignment

--- test/index-test.js
+++ test/index-test.js
@@ -1,52 +1,51 @@
-
-describe('shout(string)', function() {
-  it('receives one argument and returns it in all caps', function() {
-    expect(shout('hello')).toEqual('HELLO');
-  })
-})
-
-describe('whisper(string)', function() {
-  it('receives one argument and returns it in all lowercase', function() {
-    expect(whisper('HELLO')).toEqual('hello');
-  })
-})
-
-describe('logShout(string)', function() {
-  it('takes a string argument and logs it in all caps using console.log()', function() {
-    const spy = expect.spyOn(console, 'log').andCallThrough();
-
-    logShout('hello');
-
-    expect(spy).toHaveBeenCalledWith('HELLO');
-
-    console.log.restore();
-  })
-})
-
-describe('logWhisper(string)', function() {
-  it('takes a string argument and logs it in all lowercase using console.log()', function() {
-    const spy = expect.spyOn(console, 'log').andCallThrough();
-
-    logWhisper('HELLO');
-
-    expect(spy).toHaveBeenCalledWith('hello');
-
-    console.log.restore();
-  })
-})
-
-describe('sayHiToHeadphonedRoommate(string)', function() {
-  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
-    expect(sayHiToHeadphonedRoommate('hello')).toEqual("I can't hear you!");
-  })
-
-  it('returns "YES INDEED!" if `string` is uppercase', function() {
-    expect(sayHiToHeadphonedRoommate('HELLO')).toEqual("YES INDEED!");
-  })
-
-  it('returns "I would love to!" if `string` is "Let\'s have dinner together!"`', function () {
-    expect(sayHiToHeadphonedRoommate("Let's have dinner together!")).toEqual(
-      "I would love to!"
-    );
-  });
-})
+// Follow along with the examples here
+function doNothing() {}
+function sayHello() {
+    console.log("Hello!");
+  }
+  sayHello();
+  function sayHelloToIsabel() {
+    console.log("Hello, Isabel!");
+  }
+  sayHelloToIsabel()
+  function sayHelloToSofia() {
+    console.log("Hello, Sofia!");
+  }
+  sayHelloToSofia()
+  function sayHelloToBrendan() {
+    console.log("Hello, Brendan!");
+  }
+  sayHelloToBrendan()
+  sayHelloToIsabel();
+sayHelloToSofia();
+sayHelloToBrendan();
+function sayHelloTo(firstName) {
+    console.log(`Hello, ${firstName}!`);
+  }
+  
+  sayHelloTo("Isabel"); // "Hello, Isabel!"
+  sayHelloTo("Jane"); // "Hello, Jane!"
+  sayHelloTo("R2-D2"); // "Hello, R2-D2!"
+  sayHelloTo(1); // "Hello, 1!"
+  
+  // ^ Note that in the above, JavaScript coerces the number 1 to the string "1"
+  function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}!`);
+  }
+  say("Goodbye", "Julio");
+  say("Julio", "hello");
+  function say(greeting, firstName) {
+    console.log("firstName: ", firstName);
+    console.log("greeting: ", greeting);
+    console.log(`${greeting}, ${firstName}!`);
+  }
+  function add(x, y) {
+    return x + y;
+  }
+  console.log(add(1, 2)); 
+  function say(greeting, firstName) {
+    return `${greeting}, ${firstName}!`;
+  }
+  console.log(say("Hello", "Sofia"));
+  const sum = add(num1, num2);
+const message = `The sum of your numbers is: ${sum}.`;
